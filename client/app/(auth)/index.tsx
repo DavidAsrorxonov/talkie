c8f148cdd07732f357568be8f797e16eaa3d792d
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-06-24 22:07:35 +0900

    Refactor AuthLayout to use Stack for navigation and rename AuthIndex to AuthScreen for consistency

--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-export default function AuthIndex() {
+export default function AuthScreen() {
   return (
     <View>
       <Text>AuthIndex</Text>
